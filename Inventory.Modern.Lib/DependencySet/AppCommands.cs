4b9f8690fb2cd6b89396bf5e9b854df948f95f22
krzm <kmazanek@gmail.com>
2022-05-22 18:26:29 +0200

    Add dependency sets

---
+++ Inventory.Modern.Lib/DependencySet/AppCommands.cs
@@ -0,0 +1,40 @@
+using CRUDCommandHelper;
+using DIHelper.Unity;
+using Unity;
+
+namespace Inventory.Modern.Lib;
+
+public class AppCommands 
+    : UnityDependencySet
+{
+    public AppCommands(
+        IUnityContainer container) 
+        : base(container)
+    {
+    }
+
+    public override void Register()
+    {
+        Container
+            .RegisterSingleton<IReadCommand<ItemReadArg>, ItemReadCommand>()
+            .RegisterSingleton<IReadCommand<CategoryArgFilter>, CategoryReadCommand>()
+            .RegisterSingleton<IReadCommand<SizeArgFilter>, SizeReadCommand>()
+            .RegisterSingleton<IReadCommand<ImageArgFilter>, ImageReadCommand>()
+            .RegisterSingleton<IReadCommand<StockArgFilter>, StockReadCommand>()
+            .RegisterSingleton<IReadCommand<SizeArgFilter>, SizeReadCommand>()
+
+            .RegisterSingleton<IInsertCommand<ItemInsertArg>, ItemInsertCommand>()
+            .RegisterSingleton<IInsertCommand<CategoryArg>, CategoryInsertCommand>()
+            .RegisterSingleton<IInsertCommand<SizeArg>, SizeInsertCommand>()
+            .RegisterSingleton<IInsertCommand<ImageArg>, ImageInsertCommand>()
+            .RegisterSingleton<IInsertCommand<StockArg>, StockInsertCommand>()
+            .RegisterSingleton<IInsertCommand<SizeArg>, SizeInsertCommand>()
+
+            .RegisterSingleton<IUpdateCommand<ItemUpdateArg>, ItemUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<CategoryArgUpdate>, CategoryUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<SizeArgUpdate>, SizeUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<ImageArgUpdate>, ImageUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<StockArgUpdate>, StockUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<SizeArgUpdate>, SizeUpdateCommand>();
+    }
+}
---
+++ Inventory.Modern.Lib/DependencySet/AppMappings.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Inventory.Data;
+using Unity;
+
+namespace Inventory.Modern.Lib;
+
+public class AppMappings 
+    : ModelHelper.AppMappings
+{
+    public AppMappings(
+        IUnityContainer container)
+        : base(container)
+    {
+    }
+
+    protected override MapperConfiguration CreateMap() =>
+        new (
+        c => 
+        {
+            c.CreateMap<ItemInsertArg, Item>();
+            c.CreateMap<CategoryArg, Category>();
+            c.CreateMap<SizeArg, Size>();
+            c.CreateMap<ImageArg, Image>();
+            c.CreateMap<StockArg, Stock>();
+
+            c.CreateMap<ItemUpdateArg, ItemUpdate>();
+            c.CreateMap<CategoryArgUpdate, CategoryUpdate>();
+            c.CreateMap<SizeArgUpdate, SizeUpdate>();
+            c.CreateMap<ImageArgUpdate, ImageUpdate>();
+            c.CreateMap<StockArgUpdate, StockUpdate>();
+        });
+}
