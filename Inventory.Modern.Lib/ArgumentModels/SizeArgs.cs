a4e9a24464e4624079305fa45b6cbe3568ab809f
krzm <kmazanek@gmail.com>
2022-06-04 01:21:38 +0200

    Upd SizeArgs

--- Inventory.Modern.Lib/ArgumentModels/SizeArgs.cs
+++ Inventory.Modern.Lib/ArgumentModels/SizeArgs.cs
@@ -10,24 +10,32 @@ public class SizeArgs
 {
     private const string DimensionError = "Dimension must be greater than zero";
 
+    [Operand(nameof(LengthUnitId))
+        , Range(1, int.MaxValue, ErrorMessage = IdError)]
+    public int LengthUnitId { get; set; }
+
+    [Operand(nameof(VolumeUnitId))
+        , Range(1, int.MaxValue, ErrorMessage = IdError)]
+    public int VolumeUnitId { get; set; }
+
     [Option('l', "length")
-        , Range(0.0, int.MaxValue, ErrorMessage = DimensionError)]
+        , Range(0.0, double.MaxValue, ErrorMessage = DimensionError)]
     public double? Length { get; set; }
 
     [Option('e', "heigth")
-        , Range(0.0, int.MaxValue, ErrorMessage = DimensionError)]
+        , Range(0.0, double.MaxValue, ErrorMessage = DimensionError)]
     public double? Heigth { get; set; }
 
     [Option('d', "depth")
-        , Range(0.0, int.MaxValue, ErrorMessage = DimensionError)]
+        , Range(0.0, double.MaxValue, ErrorMessage = DimensionError)]
     public double? Depth { get; set; }
     
     [Option('i', "diameter")
-        , Range(0.0, int.MaxValue, ErrorMessage = DimensionError)]
+        , Range(0.0, double.MaxValue, ErrorMessage = DimensionError)]
     public double? Diameter { get; set; }
 
     [Option('v', "volume")
-        , Range(0.0, int.MaxValue, ErrorMessage = DimensionError)]
+        , Range(0.0, double.MaxValue, ErrorMessage = DimensionError)]
     public double? Volume { get; set; }
 
     [Option('s', "desc")
